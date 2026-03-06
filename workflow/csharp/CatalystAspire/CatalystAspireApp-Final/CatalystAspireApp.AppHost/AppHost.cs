f90d23da93861998ad23df06b28cef59a6ba80e5
Marc Duiker <marcduiker@users.noreply.github.com>
2026-03-06 13:22:47 +0100

    Catalyst Final is working with Catalyst
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- workflow/csharp/CatalystAspire/CatalystAspireApp-Final/CatalystAspireApp.AppHost/AppHost.cs
+++ workflow/csharp/CatalystAspire/CatalystAspireApp-Final/CatalystAspireApp.AppHost/AppHost.cs
@@ -1,44 +1,13 @@
-using System.Reflection;
-using CommunityToolkit.Aspire.Hosting.Dapr;
 using Diagrid.Aspire.Hosting.Catalyst;
 using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddDapr();
-
-string executingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new("Where am I?");
-
-var cachePassword = builder.AddParameter("cache-password", "zxczxc123", secret: true);
-var cache = builder
-    .AddValkey("cache", 16379, cachePassword)
-    .WithContainerName("workflow-state")
-    .WithDataVolume("workflow-state-data")
-    ;
+var catalystProject = builder.AddCatalystProject("catalyst-aspire")
+    .WithCatalystKvStore();
 
 var apiService = builder.AddProject<Projects.CatalystAspireApp_ApiService>("apiservice")
-    .WithDaprSidecar(new DaprSidecarOptions
-        {
-            LogLevel = "debug",
-            ResourcesPaths =
-            [
-                Path.Join(executingPath, "Resources"),
-            ],
-        }
-    );
-
-apiService.WaitFor(cache);
-
-builder
-    .AddContainer("diagrid-dashboard", "ghcr.io/diagridio/diagrid-dashboard:latest")
-    .WithContainerName("diagrid-dashboard")
-    .WithBindMount(Path.Join(executingPath, "Resources"), "/app/components")
-    .WithEnvironment("COMPONENT_FILE", "/app/components/statestore-dashboard.yaml")
-    .WithEnvironment("APP_ID", "diagrid-dashboard")
-    .WithHttpEndpoint(targetPort: 8080)
-    .WithReference(cache)
-    ;
-
+    .WithCatalyst(catalystProject);
 
 builder.AddProject<Projects.CatalystAspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
